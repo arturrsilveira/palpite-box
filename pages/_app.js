7aea2a779226d888f9a86465e21ac7d064d7d67b
arturrsilveira <arturrsilveira85@gmail.com>
2021-10-15 09:30:10 -0300

    TailwindCSS adicionado no projeto

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyApp = ({Component, pageProps}) => {
+  return (
+    <div>
+      <h1>MyApp</h1>
+        <Component {...pageProps} />
+    </div>
+  ) 
+}
+
+export default MyApp;
